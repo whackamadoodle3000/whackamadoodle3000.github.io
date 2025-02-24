432b2378776d2f5f12b9b2e24f8eb3b71c4619f6
Pranav Tadepalli <orca.pranav@gmail.com>
2025-02-24 13:56:53 -0500

    t

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-$(function () {
 
-    the_game = function () {
 
-        if (check_egg_hits_floor(egg1) || check_egg_hits_basket(egg1)) {
-            set_egg_to_initial_position(egg1);
-        } else {
-            egg_down(egg1);
-        }
 
-        if (check_egg_hits_floor(egg2) || check_egg_hits_basket(egg2)) {
-            set_egg_to_initial_position(egg2);
-        } else {
-            egg_down(egg2);
-        }
+var naavy = document.getElementsByClassName("navi")
+var length = naavy.length;
 
-        if (check_egg_hits_floor(egg3) || check_egg_hits_basket(egg3)) {
-            set_egg_to_initial_position(egg3);
-        } else {
-            egg_down(egg3);
-        }
+for(var i=0; i<length; i++) {
+    naavy[i].addEventListener("click", function() {
+        highlight(this);
+    });
+}
 
-        if (life > 0) {
-            anim_id = requestAnimationFrame(the_game);
-        } else {
-            stop_the_game();
-        }
-    };
+function highlight(element) {
+    for(var i=0; i<length; i++) {
+       naavy[i].classList.remove("white");
+    }
 
-    anim_id = requestAnimationFrame(the_game);
-
-});
+    element.classList.add("white");
+}
